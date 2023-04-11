ebb2b2332ae594e1fa9076ce29cd91234f5d3894
Victor Gasnet <vgasnet@atulam.fr>
2023-04-11 11:13:42 +0200

    First Commit

---
+++ TestCommande.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace AtulamAdmin
+{
+    partial class TestCommande
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grille = new ControlsBase.Atulam_Grille();
+            this.gv = new DevExpress.XtraGrid.Views.Grid.GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grille)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grille
+            // 
+            this.grille.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grille.EnabledControl = true;
+            this.grille.Location = new System.Drawing.Point(0, 10);
+            this.grille.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.Flat;
+            this.grille.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.grille.MainView = this.gv;
+            this.grille.Name = "grille";
+            this.grille.Size = new System.Drawing.Size(1519, 773);
+            this.grille.TabIndex = 5;
+            this.grille.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gv});
+            // 
+            // gv
+            // 
+            this.gv.GridControl = this.grille;
+            this.gv.Name = "gv";
+            // 
+            // TestCommande
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1519, 783);
+            this.Controls.Add(this.grille);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MinimumSize = new System.Drawing.Size(395, 289);
+            this.Name = "TestCommande";
+            this.Text = "TestCommande";
+            this.Controls.SetChildIndex(this.grille, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.grille)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ControlsBase.Atulam_Grille grille;
+        private DevExpress.XtraGrid.Views.Grid.GridView gv;
+    }
+}
---
+++ TestCommande.cs
@@ -0,0 +1,81 @@
+﻿using Atulam_Appels.Interfaces;
+using Atulam_Appels.Services;
+using ControlsBase;
+using DevExpress.XtraGrid.Views.Grid;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtulamAdmin
+{
+    public partial class TestCommande : Atulam_Fiche
+    {
+
+        public class ContenuGrille
+        {
+            public string Code { get; set; }
+
+            public List<(string Libelle, int Valeur)> Valeurs { get; set; } = new List<(string Libelle, int Valeur)>();
+        }
+
+        List<ContenuGrille> source = new List<ContenuGrille>();
+
+        IAtulamApiGeneral atulamApiGeneral = new AtulamApiGeneral();
+
+        public TestCommande()
+        {
+            InitializeComponent();
+            Load += TestCommande_Load;
+        }
+
+        private void TestCommande_Load(object sender, EventArgs e)
+        {
+
+            List<String> indicateurs = new List<string>() { "A1", "A2" }; // Define in DataBase, the developper don't know how many 'indicateurs' the source have
+
+            // For each Code, we have all the 'indicateurs' with a value
+            source.Add(new ContenuGrille() { Code = "A", Valeurs = GenerateValue(indicateurs) });
+            source.Add(new ContenuGrille() { Code = "B", Valeurs = GenerateValue(indicateurs) });
+
+            grille.DataSource = source;
+            gv.Columns.Clear();
+            int indice = 0;
+            grille.AjouteColonne(gv, "Code", "Code", indice++);
+            //foreach (var ind in source[0].Valeurs)
+            //{
+            //    DevExpress.XtraGrid.Columns.GridColumn gc = AjouteColonneGeneral(gv, "Valeurs[" + ind.Libelle + "]", ind.Libelle);
+
+            //    //gc.FieldName = ""  ?? 
+            //}
+        }
+
+        private DevExpress.XtraGrid.Columns.GridColumn AjouteColonneGeneral(GridView myView, string name, string caption, string field)
+        {
+            return new DevExpress.XtraGrid.Columns.GridColumn()
+            {
+                Name = name,
+                Caption = caption,
+                FieldName = field
+            };
+        }
+
+
+
+        private List<(string Libelle, int Valeur)> GenerateValue(List<string> indicateurs)
+        {
+            var rand = new Random();
+
+            List<(string Libelle, int Valeur)> values = new List<(string Libelle, int Valeur)>();
+            foreach (var ind in indicateurs)
+                values.Add((ind, rand.Next(0, 10)));
+            return values;
+        }
+    }
+}
